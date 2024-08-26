487b23d6af54b621d8b5d2f70099d3d9a9c41243
Ali Tim <ali.tim03.at@gmail.com>
2024-08-26 17:24:57 +0500

    start back

---
+++ server/main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+from books.views import auth
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', auth),
+    re_path('', include('social_django.urls', namespace='social'))
+]
