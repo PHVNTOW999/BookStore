133c71ce120e67a205761ef072641cb3cf4cdc0f
Ali Tim <ali.tim03.at@gmail.com>
2024-09-12 10:05:49 +0500

    update

---
+++ client/src/views/AuthView.vue
@@ -0,0 +1,14 @@
+<script>
+import LoginForm from "@/components/form/LoginForm.vue";
+
+export default {
+  name: "AuthView",
+  components: {LoginForm}
+}
+</script>
+
+<template>
+  <div class="auth">
+    <LoginForm />
+  </div>
+</template>
